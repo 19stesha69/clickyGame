47cfb64bc62d68ce12bb1d34023171d55a9317b4
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-14 14:50:47 -0700

    pull files out of my-app folder

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Main = (props) => {
+    return (
+        <main class="container">
+            
+        </main>
+    );
+}
+
+
+export default Main;
