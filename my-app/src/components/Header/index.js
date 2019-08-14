0616d8b628273db4b451aab90be4c449e62bd593
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-14 14:01:37 -0700

    add nav and header components

---
+++ my-app/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header class="header">
+            <h1>Clicky Game!</h1>
+            <h2>Click on any image to earn points, but don't click on any one more than once!</h2>
+        </header>
+    );
+}
+
+export default Header;
---
+++ my-app/src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Nav = () => {
+    return (
+        <nav class="navbar">
+            <ul>
+                <li>
+                    <a href="#">Clicky Game</a>
+                </li>
+                <li>Click an image to begin!</li>
+                <li></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
