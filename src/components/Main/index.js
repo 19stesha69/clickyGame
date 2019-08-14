9fb93cd9505d705d86d1192d7d9cfddda058d08b
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-14 15:18:21 -0700

    add image folder

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 const Main = (props) => {
     return (
         <main class="container">
-            
+            <div role="img" aria-label="click item" class="click-item">
+                <img alt={props.name} src={props.image}></img>
+            </div>
         </main>
     );
 }
