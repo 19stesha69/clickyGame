aa7b0f0a15aa5029fd73bb7da2c4b403aebfada8
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-15 09:14:42 -0700

    add css to main component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
+import Main from './components/Main'
 
 function App() {  
   return(
     <div>
       <Nav />
       <Header />
+      <Main />
     </div> 
   );
 }
