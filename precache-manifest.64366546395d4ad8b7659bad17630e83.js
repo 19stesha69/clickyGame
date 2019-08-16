26a699665f34089d555bf47e00ee0de72054be3a
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-08-15 23:33:17 -0700

    Updates

---
+++ precache-manifest.64366546395d4ad8b7659bad17630e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf26074bcd9ce4b7f0f7d90991a98dc",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e2bf69581d1ff687c380",
+    "url": "/clickyGame/static/css/main.95cc6915.chunk.css"
+  },
+  {
+    "revision": "d53ed0b64edb374bc196",
+    "url": "/clickyGame/static/js/2.a3a7ffee.chunk.js"
+  },
+  {
+    "revision": "e2bf69581d1ff687c380",
+    "url": "/clickyGame/static/js/main.02861d80.chunk.js"
+  },
+  {
+    "revision": "40df9540b2c3100b6dd7",
+    "url": "/clickyGame/static/js/runtime~main.13e4c39a.js"
+  }
+]);
